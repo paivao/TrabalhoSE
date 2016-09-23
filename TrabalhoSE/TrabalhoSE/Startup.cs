a6bec57744e3e9a6ed78ad3d7f4af67af9395a7e
paivao <08.paiva@gmail.com>
2016-09-23 15:32:50 -0300

    Start ASP NET server

---
+++ TrabalhoSE/TrabalhoSE/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrabalhoSE.Startup))]
+namespace TrabalhoSE
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
