aba3089120779864983bb63ebf2837e42886a20e
paivao <08.paiva@gmail.com>
2016-09-29 22:58:30 -0300

    Started Arduino Integration.

---
+++ TrabalhoSE/TrabalhoSE/Controllers/ArduinoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrabalhoSE.Filters;
+using TrabalhoSE.Models;
+
+namespace TrabalhoSE.Controllers
+{
+    [AuthorizeIPAddress]
+    public class ArduinoController : Controller
+    {
+        // GET: Arduino
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Arduino/Sensor?temp=...
+        public ActionResult Sensor(float? temp, float? hum, float? tension)
+        {
+            SensorData sensors = new SensorData { Temperature = temp, Humidity = hum, Power = tension, Time = DateTime.Now };
+            (new ArduinoContext()).SensorData.Add(sensors);
+            return new HttpStatusCodeResult(200);
+            //return View();
+        }
+    }
+}
---
+++ TrabalhoSE/TrabalhoSE/Models/ArduinoModels.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace TrabalhoSE.Models
+{
+    public class SensorData
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public float? Temperature { get; set; }
+        public float? Humidity { get; set; }
+        public float? Power { get; set; }
+        public DateTime Time { get; set; }
+    }
+
+    public class ArduinoContext : DbContext
+    {
+        public ArduinoContext() : base("DefaultConnection") { }
+
+        public DbSet<SensorData> SensorData { get; set; }
+    }
+}
